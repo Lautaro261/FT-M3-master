5f1a0e053c5dcca9fb64d99f5e59d9572ec4fdfb
Lautaro261 <nahuelygoofy@gmail.com>
2022-11-09 02:29:48 -0300

    app lista

--- 03-WebServer/homework/showByName/app.js
+++ 03-WebServer/homework/showByName/app.js
@@ -1,4 +1,16 @@
 var fs  = require("fs")
 var http  = require("http")
 
-// Escribí acá tu servidor
+
+
+http.createServer((request, response)=>{
+    fs.readFile(`${__dirname}/images/${request.url}.jpg`, (err, data)=>{
+        if(err){
+            response.writeHead(400, {'Content-Type':'text-plain'});
+            response.end('no papi')
+        }else{
+            response.writeHead(200, {'Content_type':'image'});
+            response.end(data);
+        }
+    })
+}).listen(3000, '127.0.0.1');   
